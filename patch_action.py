1c25eba8818e38affb22077092b14522ec9eed10
Sakib Sakfi <ananno.sakib@gmail.com>
2026-03-05 04:39:36 +0600

    refactor: replace python patcher with native bash sed commands

--- patch_action.py
+++ patch_action.py
@@ -38,66 +38,46 @@ for line in lines:
 output_lines = []
 for line in trimmed_lines:
     safe_line = line.replace('\\', '\\\\').replace('"', '\\"')
-    output_lines.append(f'ui_print "{safe_line}"')
+    output_lines.append(f'        ui_print "{safe_line}";')
 
 # Read action.yml
 with open('.github/actions/action.yml', 'r', encoding='utf-8') as f:
     action_content = f.read()
 
-# Build python patcher that runs during GitHub Action
-workflow_patch = '''
-        cat << 'EOF' > patch_anykernel.py
-import sys
-import re
-
-with open('AnyKernel3/anykernel.sh', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-# Replace texts
-content = content.replace('WildKernels Telegram Channel:', 'SakFi Kernel Releases:')
-content = content.replace('https://t.me/WildKernels', 'https://github.com/sakfi/OP_KSUN_FS')
-content = content.replace('WildKernels Website:', '')
-content = content.replace('https://wildkernels.dev', '')
-content = content.replace('Wild_KSU GitHub Repository:', '')
-content = content.replace('https://github.com/WildKernels/Wild_KSU', '')
-content = content.replace('KernelSU-Next fork focused on customization and root-hiding features!', 'OnePlus kernels tailored with KernelSU Next and SUSFS.')
-content = content.replace('GKI_KernelSU_SUSFS GitHub Repository:', '')
-content = content.replace('https://github.com/WildKernels/GKI_KernelSU_SUSFS', '')
-content = content.replace('GKI kernels with KernelSU and SUSFS.', '')
-content = content.replace('OnePlus_KernelSU_SUSFS GitHub Repository:', '')
-content = content.replace('https://github.com/WildKernels/OnePlus_KernelSU_SUSFS', '')
-content = content.replace('OnePlus kernels with KernelSU and SUSFS.', '')
-
-content = content.replace('Wild Kernels by TheWildJames aka Morgan Weedman', 'SakFi OP Kernels by sakfi')
-content = content.replace('AnyKernel3 by osm0sis @ xda-developers', 'AnyKernel3 by osm0sis @ xda-developers\\\\nui_print "Wild Kernels by TheWildJames"\\\\nui_print "& fatalcoder524"')
-content = content.replace('Wild Kernels Supported', 'SakFi Kernels Supported')
-
-# Inject ASCII replacing 'Done!'
-ascii_commands = """%s"""
-
-lines = content.split('\\n')
-for i, line in enumerate(lines):
-    if 'ui_print "Done!";' in line:
-        art_lines = [l+";" for l in ascii_commands.splitlines() if l.strip()]
-        lines.insert(i, '\\n'.join(art_lines))
-        break
-
-content = '\\n'.join(lines)
-content = content.replace('ui_print "Done!";', 'ui_print "Done! Good Luck with your new Kernel!!";')
-
-# Remove duplicate empty ui_prints caused by deleted urls
-content = re.sub(r'(ui_print "";\\s*){2,}', 'ui_print "";\\n', content)
-
-with open('AnyKernel3/anykernel.sh', 'w', encoding='utf-8') as f:
-    f.write(content)
-EOF
-        python3 patch_anykernel.py
-        rm patch_anykernel.py
-'''
+# Build bash patcher that runs during GitHub Action
+workflow_patch = """        AK3="AnyKernel3/anykernel.sh"
+        
+        sed -i 's/WildKernels Telegram Channel:/SakFi Kernel Releases:/g' "$AK3"
+        sed -i 's|https://t.me/WildKernels|https://github.com/sakfi/OP_KSUN_FS|g' "$AK3"
+        sed -i 's/WildKernels Website://g' "$AK3"
+        sed -i 's|https://wildkernels.dev||g' "$AK3"
+        sed -i 's/Wild_KSU GitHub Repository://g' "$AK3"
+        sed -i 's|https://github.com/WildKernels/Wild_KSU||g' "$AK3"
+        sed -i 's/KernelSU-Next fork focused on customization and root-hiding features!/OnePlus kernels tailored with KernelSU Next and SUSFS./g' "$AK3"
+        sed -i 's/GKI_KernelSU_SUSFS GitHub Repository://g' "$AK3"
+        sed -i 's|https://github.com/WildKernels/GKI_KernelSU_SUSFS||g' "$AK3"
+        sed -i 's/GKI kernels with KernelSU and SUSFS.//g' "$AK3"
+        sed -i 's/OnePlus_KernelSU_SUSFS GitHub Repository://g' "$AK3"
+        sed -i 's|https://github.com/WildKernels/OnePlus_KernelSU_SUSFS||g' "$AK3"
+        sed -i 's/OnePlus kernels with KernelSU and SUSFS.//g' "$AK3"
+        sed -i 's/Wild Kernels by TheWildJames aka Morgan Weedman/SakFi OP Kernels by sakfi/g' "$AK3"
+        sed -i 's/AnyKernel3 by osm0sis @ xda-developers/AnyKernel3 by osm0sis @ xda-developers\\nui_print "Wild Kernels by TheWildJames"\\nui_print "\\& fatalcoder524"/g' "$AK3"
+        sed -i 's/Wild Kernels Supported/SakFi Kernels Supported/g' "$AK3"
+        
+        cat << 'ASCIIEOF' > ascii_art.txt
+%s
+        ASCIIEOF
+        
+        sed -i -e '/ui_print "Done!";/r ascii_art.txt' -e '/ui_print "Done!";/d' "$AK3"
+        echo 'ui_print "Done! Good Luck with your new Kernel!!";' >> "$AK3"
+        
+        python3 -c "import re; f=open('$AK3', 'r'); c=f.read(); f.close(); c=re.sub(r'(ui_print \\"\\";\\\\s*){2,}', 'ui_print \\"\\";\\\\n', c); f=open('$AK3', 'w'); f.write(c)"
+        rm ascii_art.txt
+"""
 
 # Escape correctly for YAML and python string
-formatted_ascii = '\\n'.join(output_lines)
-patch_str = workflow_patch % formatted_ascii.replace('\\', '\\\\')
+formatted_ascii = '\n'.join(output_lines)
+patch_str = workflow_patch % formatted_ascii
 
 start_idx = action_content.find('# --- Patch AnyKernel3 flashing output for SakFi ---')
 end_idx = action_content.find('# --------------------------------------------------', start_idx)
@@ -109,3 +89,4 @@ if start_idx != -1 and end_idx != -1:
     print("Successfully patched action.yml content.")
 else:
     print("ERROR: Could not find patch block in action.yml. Make sure it exists.")
+
